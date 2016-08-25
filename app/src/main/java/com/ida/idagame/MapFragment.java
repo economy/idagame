b9731f39cb37784049c9ebe260c591d500b0101f
economy <the.economy@gmail.com>
2016-08-25 14:44:16 -0700

    places works for banks now

--- app/src/main/java/com/ida/idagame/MapFragment.java
+++ app/src/main/java/com/ida/idagame/MapFragment.java
@@ -10,8 +10,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.util.Log;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +39,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.location.places.Places;
 
 /**
  * Created by economy on 8/24/16.
@@ -39,6 +54,8 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
 
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
+    GoogleMap mGoogleMap;
+    HashMap<String, String> mMarkerPlaceLink = new HashMap<String, String>();
 
     private final int[] MAP_TYPES = { GoogleMap.MAP_TYPE_SATELLITE,
             GoogleMap.MAP_TYPE_NORMAL,
@@ -58,9 +75,12 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
                 .addConnectionCallbacks( this )
                 .addOnConnectionFailedListener( this )
                 .addApi( LocationServices.API )
+                .addApi( Places.GEO_DATA_API )
+                .addApi( Places.PLACE_DETECTION_API )
                 .build();
 
         initListeners();
+
     }
 
     private void initListeners() {
@@ -102,9 +122,10 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     }
 
     private void initCamera( Location location ) {
+
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
         CameraPosition position = CameraPosition.builder()
-                .target( new LatLng( location.getLatitude(),
-                        location.getLongitude() ) )
+                .target( latLng )
                 .zoom( 16f )
                 .bearing( 0.0f )
                 .tilt( 0.0f )
@@ -117,15 +138,165 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         getMap().setTrafficEnabled( false );
         getMap().setMyLocationEnabled( true );
         getMap().getUiSettings().setZoomControlsEnabled( true );
+
+        String type = "bank";
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        sb.append("location="+location.getLatitude()+","+location.getLongitude());
+        sb.append("&radius=5000");
+        sb.append("&types="+type);
+        sb.append("&sensor=true");
+        sb.append("&key=AIzaSyC0pS-D86ssdh7oF3E5lnzuaJ0vWq-wCTY");
+
+        // Creating a new non-ui thread task to download Google place json data
+        PlacesTask placesTask = new PlacesTask();
+
+        // Invokes the "doInBackground()" method of the class PlaceTask
+        System.out.println(sb.toString());
+        placesTask.execute(sb.toString());
+
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         //Create a default location if the Google API Client fails. Placing location at Googleplex
-        mCurrentLocation = new Location( "" );
-        mCurrentLocation.setLatitude( 37.422535 );
-        mCurrentLocation.setLongitude( -122.084804 );
+        mCurrentLocation = new Location("");
+        mCurrentLocation.setLatitude(37.422535);
+        mCurrentLocation.setLongitude(-122.084804);
         initCamera(mCurrentLocation);
+    }
+
+    private String downloadUrl(String strUrl) throws IOException{
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+            data = sb.toString();
+            System.out.println(sb.toString());
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exc while dloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+    /** A class, to download Google Places */
+    private class PlacesTask extends AsyncTask<String, Integer, String>{
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try{
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result){
+            ParserTask parserTask = new ParserTask();
+
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParseTask
+            parserTask.execute(result);
+        }
+    }
+
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String,String>>>{
+
+        JSONObject jObject;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected List<HashMap<String,String>> doInBackground(String... jsonData) {
+
+            List<HashMap<String, String>> places = null;
+            PlaceJsonParser placeJsonParser = new PlaceJsonParser();
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+
+                /** Getting the parsed data as a List construct */
+                places = placeJsonParser.parse(jObject);
+
+            }catch(Exception e){
+                Log.d("Exception",e.toString());
+            }
+            return places;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(List<HashMap<String,String>> list){
+
+            // Clears all the existing markers
+            //mGoogleMap.clear();
+
+            for(int i=0;i<list.size();i++){
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                // Getting name
+                String name = hmPlace.get("place_name");
+
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+
+                LatLng latLng = new LatLng(lat, lng);
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                //This will be displayed on taping the marker
+                markerOptions.title(name + " : " + vicinity);
+
+                // Placing a marker on the touched position
+                Marker m = getMap().addMarker(markerOptions);
+
+                // Linking Marker id and place reference
+                mMarkerPlaceLink.put(m.getId(), hmPlace.get("reference"));
+            }
+        }
     }
 
     @Override
