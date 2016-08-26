8c6e2b77dc22da5e4e974ac0aedb0e7057213076
Reza F <rfarsian@gmail.com>
2016-08-25 17:18:14 -0700

    changed to User and Business

--- app/src/main/java/com/ida/idagame/Business.java
+++ app/src/main/java/com/ida/idagame/Business.java
@@ -1,7 +1,29 @@
 package com.ida.idagame;
 
+import java.util.Random;
+
 /**
  * Created by rfarsian on 8/25/16.
  */
 public class Business {
+
+    protected static float difficulty;
+    protected static float reward;
+    public static 
+
+    public Business() {
+        difficulty = 0;
+        reward = 0;
+    }
+
+    public Business(double latitude, double longitude){
+        int rewardSeed = (int) (Math.pow(10, 6) * Math.round(latitude));
+        int difficultySeed = (int) (Math.pow(10, 6) * Math.round(longitude));
+        Random rewardGenerator = new Random(rewardSeed);
+        Random difficultyGenerator = new Random(difficultySeed);
+
+        float rewardProb = rewardGenerator.nextInt(100);
+        float difficultyProb = difficultyGenerator.nextInt(100);
+    }
+
 }
